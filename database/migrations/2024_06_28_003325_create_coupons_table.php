2871c871fb6718402bee7f748b083a9f83540ce9
Restia <thongphamminh3@gmail.com>
2024-06-28 08:58:31 +0700

    update migrate

---
+++ database/migrations/2024_06_28_003325_create_coupons_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 30)->unique();
+            $table->string('discount', 3);
+            $table->date('expiry_date');
+            $table->decimal('min_value', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
